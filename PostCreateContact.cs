c56672c2981a3cffcfca0540156cf165a2c254fb
kasyoki.justus <justuskasyoki57@gmail.com>
2022-05-23 11:27:06 +0300

    Add project files.

---
+++ PostCreateContact.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace Postcreatecontact
+{
+    public class PostCreateContact : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)
+            serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data
+            //passed in the message request.
+
+            if (context.InputParameters.Contains("Target"))
+            {
+
+                // Obtain the target entity from the input parameters.
+                Entity entity = (Entity)context.InputParameters["Target"];
+                try
+                {
+
+                    // Create a task activity to follow up with the account customer in 7 days
+                    Entity followup = new Entity("task");
+                    followup["subject"] = "Send e-mail to the new customer.";
+                    followup["description"] =
+                       "Follow up with the customer. Check if there are any new issues that need resolution.";
+
+
+                    followup["scheduledstart"] = DateTime.Now;
+                    followup["scheduledend"] = DateTime.Now.AddDays(2);
+                    followup["category"] = context.PrimaryEntityName;
+
+                    // Refer to the contact in the task activity.
+                    if (context.OutputParameters.Contains("id"))
+                    {
+                        Guid regardingobjectid = new Guid(context.OutputParameters["id"].ToString());
+                        string regardingobjectidType = "contact";
+                        followup["regardingobjectid"] = new EntityReference(regardingobjectidType, regardingobjectid);
+                    }
+
+                    // Obtain the organization service reference.
+                    IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService
+                       (typeof(IOrganizationServiceFactory));
+                    IOrganizationService service =
+                       serviceFactory.CreateOrganizationService(context.UserId);
+
+                    // Create the followup activity
+                    service.Create(followup);
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidPluginExecutionException(ex.Message);
+                }
+            }
+            else { return; };
+        }
+    }
+}
